935821190b6ee11006dfec8686cb3068693fa69c
MarioGuTe <mariogutierreztello@gmail.com>
2024-04-01 02:34:53 -0300

    add class validators

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateCatDto {
+  @IsNotEmpty()
   id: number;
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
   age: number;
+  @IsNotEmpty()
   breed: string;
 }
